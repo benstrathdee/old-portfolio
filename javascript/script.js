e69545d2e1247ecb816c5d4e48bbd3954a6d3436
Ben Strathdee <ben.strathdee@live.com.au>
2021-11-12 07:36:52 +1100

    changed layout, fixed styling

--- javascript/script.js
+++ javascript/script.js
@@ -12,3 +12,12 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
+
+// This worked, but I couldn't change it back after scrolling past, so unused
+// function enlarge(link, link2) {
+//     if (window.pageYOffset > link.offsetTop - 150) {
+//         link2.classList.add("a--current");
+//     } else {
+//         link2.classList.remove("a--current");
+//     }
+// }
